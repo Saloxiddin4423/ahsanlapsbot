a4d716004607720b72a01b06554fd86643f31e36
firdavs abdullajonov <turgunovsaloxiddin018@gmail.com>
2025-02-21 02:16:32 +0500

    tedt

--- dotenv.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-  FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-};
